b31cd2a7df1d323fa2b2d28dd13481b58fee6bef
Hasib Ullah <hasib.ascend@gmail.com>
2025-07-20 15:18:41 +1000

    added the Transformer block class

--- bigram_model.py
+++ bigram_model.py
@@ -122,6 +122,22 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
     
+class Block(nn.Module):
+    # THIS IS A TRANSFORMER BLOCK: COMMUNICATION FOLLOWED BY COMPUTATION
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+    
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+    
 
 
 
