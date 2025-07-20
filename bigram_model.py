55308188633bc72cdbd4f2f117a5f2b604b7bf84
Hasib Ullah <hasib.ascend@gmail.com>
2025-07-20 15:19:49 +1000

    minor code change

--- bigram_model.py
+++ bigram_model.py
@@ -114,7 +114,7 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd)
+            nn.Linear(n_embd, 4 * n_embd),
             nn.Relu(),
             nn.Linear(4 * n_embd, n_embd),
             nn.dropout(dropout)
